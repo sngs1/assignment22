392c906afb2ac85cc08f57ee132416876d5b2c9a
GANGA C K <kochupotty@gmail.com>
2016-11-03 21:07:26 +0530

    assignment2_2

---
+++ a22.java
@@ -0,0 +1,27 @@
+package a3;
+
+public class a22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1;i<6;i++)
+		{
+			if(i==1||i==5)
+			{
+			System.out.println("*_______*");
+			}
+			else if (i==2||i==4)
+			{
+				System.out.println("__*___*");
+				
+			}
+			else
+			{
+				System.out.println("____*");
+			}
+						
+		}
+	}
+
+}
